e30775140c33eca3d43471f7a7e3caf7b75166fb
PriyankaLandge <landgepriyanka84@gmail.com>
2021-11-21 00:40:35 +0530

    add to cart

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -1,3 +1,4 @@
+const array =[];
 fetch('https://fakestoreapi.com/products').then((data)=>{
     // console.log(data);
     return data.json();
@@ -7,7 +8,7 @@ fetch('https://fakestoreapi.com/products').then((data)=>{
       completedata.map((values)=>{
       data1+=`<div class="card">
     
-       <input type="checkbox" class="larger" >
+       <input type="checkbox" value="${values.title}" id="${values.id}" onclick="addToCart(value,id)">
        <h1 class="title">${values.title}</h1>
        <img src="${values.image}" alt="img" class="images">
        
@@ -21,3 +22,48 @@ fetch('https://fakestoreapi.com/products').then((data)=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+//function to add selected items in array or cart
+function addToCart(product,Id){
+    console.log('after fetch call add');
+    console.log(product);
+    var checkbox = document.getElementById(Id);
+    if(checkbox.checked == true){
+        array.push(product);
+    }else{
+        removeItem(array,product);
+    }
+    showcart();
+}
+
+//function to show all selected item on html page
+function showcart(){
+
+     let output = ``;
+     let size = array.length;
+     if(size == 0){
+         output +=`
+         <div class="cart">
+         <h3>Empty Cart<h3>
+         </div>`;
+     }
+     else{
+         for(i=0;i<size;i++){
+             output +=`
+             <div class="cart">
+             ${(i+1)} -- ${array[i]}
+             </div>`;
+         }
+     }
+     document.getElementById(cart.id).innerHTML = output;
+}
+
+//function to remove unchecked items from array or cart
+function removeItem(array,item){
+    for(var i in array){
+        if(array[i]==item){
+            array.splice(i,1);
+            break;
+        }
+    }
+}
