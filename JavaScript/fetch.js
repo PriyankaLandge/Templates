fb3010c99794c4efc0b57bc956a8e02cd1b4683c
Priyanka Landge <78359654+PriyankaLandge@users.noreply.github.com>
2021-11-21 10:15:21 +0530

    Update fetch.js

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -21,3 +21,47 @@ fetch('https://fakestoreapi.com/products').then((data)=>{
 }).catch((err)=>{
     console.log(err);
 })
+//function to add selected items in array or cart
+function addToCart(product,Id){
+    console.log('after fetch call add');
+    console.log(product);
+    var checkbox = document.getElementById(Id);
+    if(checkbox.checked == true){
+        array.push(product);
+    }else{
+        removeItem(array,product);
+    }
+    showcart();
+}
+
+//function to show all selected item on html page
+function showcart(){
+
+     let output = ``;
+     let size = array.length;
+     if(size == 0){
+         output +=`
+         <div class="cart">
+         <h3>Empty Cart<h3>
+         </div>`;
+     }
+     else{
+         for(i=0;i<size;i++){
+             output +=`
+             <div class="cart">
+             ${(i+1)} -- ${array[i]}
+             </div>`;
+         }
+     }
+     document.getElementById(cart.id).innerHTML = output;
+}
+
+//function to remove unchecked items from array or cart
+function removeItem(array,item){
+    for(var i in array){
+        if(array[i]==item){
+            array.splice(i,1);
+            break;
+        }
+    }
+}
